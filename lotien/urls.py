f2d12d7f87347a1e8fe90e6351205696522a9cf2
Arcady Chumachenko <arcady.chumachenko@gmail.com>
2015-08-23 22:34:53 +0100

    Captcha added

--- lotien/urls.py
+++ lotien/urls.py
@@ -1,9 +1,9 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import include, url, patterns
 from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = patterns('',
     url(r'^$', 'shop.views.home', name='home'),
     url(r'^collection/(?P<pk>\d+)/$', 'shop.views.collection', name='collection'),
     url(r'^collection/(?P<collection_pk>\d+)/(?P<pk>\d+)/$', 'shop.views.flower', name='flower'),
@@ -14,6 +14,6 @@ urlpatterns = [
     url(r'^sirtrevor/', include('sirtrevor.urls')),
 
     url(r'^', 'pages.views.page', name='page'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + [
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + patterns('',
     url(r'^captcha/', include('captcha.urls')),
-]
+)
